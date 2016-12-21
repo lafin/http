a79f0d18be1cc85943acd2f799ca384d25a2ef38
Kuvakin Sergey <kuvakin@gmail.com>
2016-12-22 00:29:27 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package httpclient
+
+import (
+	"crypto/tls"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/cookiejar"
+	"sync"
+	"time"
+)
+
+var once sync.Once
+var client *http.Client
+
+// GetData - get data by an url
+func GetData(url string) ([]byte, error) {
+	client := Client()
+	response, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+// Client - get instance of http client
+func Client() *http.Client {
+	once.Do(func() {
+		transport := &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout: time.Second * 10,
+			}).Dial,
+			TLSHandshakeTimeout: time.Second * 5,
+			TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
+			DisableKeepAlives:   true,
+		}
+		cookieJar, _ := cookiejar.New(nil)
+		client = &http.Client{
+			Timeout:   time.Second * 300,
+			Transport: transport,
+			Jar:       cookieJar,
+		}
+	})
+
+	return client
+}
