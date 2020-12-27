0118726eddc9430d5c1151b32c6bd750be233610
Sergei Kuvakin <sergey.kuvakin@coolbet.com>
2020-12-27 23:58:41 +0200

    feat: added post

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
+// Package http handle work with http
 package http
 
 import (
+	"fmt"
+	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -14,7 +17,7 @@ var client *http.Client
 
 // Get - wrapper to execute http GET request
 func Get(url string, headers map[string]string) ([]byte, error) {
-	client := Client()
+	client = Client()
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
@@ -26,12 +29,40 @@ func Get(url string, headers map[string]string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+	if res.StatusCode >= 400 {
+		return nil, fmt.Errorf("%d", res.StatusCode)
+	}
 	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
+	response, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
-	return body, nil
+	return response, nil
+}
+
+// Post - wrapper to execute http POST request
+func Post(url string, body io.Reader, headers map[string]string) ([]byte, error) {
+	client = Client()
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode >= 400 {
+		return nil, fmt.Errorf("%d", res.StatusCode)
+	}
+	defer res.Body.Close()
+	response, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
 }
 
 // Client - get instance of http client
